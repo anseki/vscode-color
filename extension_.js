4de45f3a39d5834a0b249c56609bef478ee45d6e
anseki <anseki@users.noreply.github.com>
2016-02-23 01:38:48 +0900

    Fix: .vscodeignore Add: `scripts` sim

---
+++ extension_.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const
+  vscode = require('vscode'),
+  packageInfo = require('./package'),
+  main = require(packageInfo.extensionMain);
+
+// [postinstall]
+
+// Since `scripts.postinstall` of `package.json` is not supported, simulate it.
+
+try {
+  let fs = require('fs'), pathUtil = require('path');
+  fs.renameSync(
+    pathUtil.join(__dirname, 'lib/app.asar_'),
+    pathUtil.join(__dirname, 'lib/app.asar'));
+
+  fs.writeFileSync(__filename,
+    fs.readFileSync(__filename, {encoding: 'utf8'})
+      .replace(/\/\/ *\[postinstall\][\s\S]*?\/\/ *\[\/postinstall\]\s*/, ''));
+} catch (error) { /* ignore */ }
+
+// [/postinstall]
+
+exports.activate = context => {
+  packageInfo.contributes.commands.forEach(commandReg => {
+    context.subscriptions.push(
+      vscode.commands.registerCommand(
+        commandReg.command, main[commandReg.command.split('.').slice(-1)]));
+  });
+};
+
+exports.deactivate = () => { if (main.deactivate) { main.deactivate(); } };
